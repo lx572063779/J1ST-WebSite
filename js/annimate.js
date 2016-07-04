68cb0c4e24713eb05b2e0a47e50061fac230c285
chenjunwei <jwchen@zeninfor.com>
2016-07-04 15:52:36 +0800

    全部二级页面完成

--- js/annimate.js
+++ js/annimate.js
@@ -1,3 +1,15 @@
-/**
- * Created by ZENIN on 2016/6/30.
- */
+var scroll=0;
+var animate=$("#dollar-animate-2");
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if(scroll>360){
+            scroll=0;
+        }
+        scroll+=10;
+        scrollball();
+    });
+    function scrollball() {
+        animate.css("transform","rotateZ"+"("+scroll+"deg"+")");
+    }
+
+});
--- js/config.js
+++ js/config.js
@@ -2,14 +2,23 @@ var winHeight=window.screen.height;
 var winWidth=window.screen.width;
 var imgContainer=$(".adver-img-container");
 var header=$("header");
+var advertext=$("#adver-text");
+var adverlogin=$("#adver-text-log-in");
+var thimg=$("#th-img");
+var registerdiv=$("#register-div");
+
 window.onload=function () {
     if(winWidth<768){
         imgContainer.css("height",winHeight*706/777);
         header.css("line-height",(winHeight*71/777)+"px");
         header.css("height",winHeight*71/777);
+        advertext.css("height",winHeight*6/7);
+        adverlogin.css("height",winHeight/7);
+        thimg.css("height",winHeight*6/8);
+        registerdiv.css("height",winHeight/4);
         $("#bottom-adver-img").css("height",winHeight*706/777)
     }
+};
 
-}
 
 
